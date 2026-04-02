b40dcfcd7df601c10eb06677d4294dcf2466337a
som <somparashar007@gmail.com>
2026-04-02 18:51:59 +0530

    05_objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,22 @@
+const { StrictMode } = require("react");
+
+const city = {
+
+    cityname: "Delhi",
+    population: "15 million",
+    famousfor: "food and hangout locations"
+}
+
+// city.famousfor
+
+const {famousfor} = city
+
+console.log(famousfor);
+
+//json api example
+//{
+  //  name: Som,
+    //age: 22,
+    //emailid: som123@gmail.com,
+
+//}
