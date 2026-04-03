687a690931cde10537469f0d295d22bfdc2390ab
som <somparashar007@gmail.com>
2026-04-03 18:36:25 +0530

    functions 2

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,31 @@
+
+function calculateCartPrice(val1,val2, ... num1){
+
+    return num1
+
+}
+console.log(calculateCartPrice(100,500,1000,2000))
+
+const user = {
+    username: "som",
+    age: 22
+}
+
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and his age is ${anyobject.age}`);
+}
+
+handleobject({
+    username: "som",
+    age: 22
+})
+
+const NewArray = [100,200,300,400]
+
+function returnsecondvalue(fetchArray){
+
+    return fetchArray[1]
+}
+
+// console.log(returnsecondvalue(NewArray));
+console.log(returnsecondvalue([100,200,300,400]));
