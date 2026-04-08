b8f12160fb648a57937ac6003a219bc640968d3b
som <somparashar007@gmail.com>
2026-04-08 22:16:24 +0530

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,22 @@
+
+// if
+
+const isUserloggedIn = true
+
+const temprature = 45
+
+if (temprature === 45){
+
+    console.log("temprature is less than 50");
+}
+
+else{
+    console.log("temprature is greater than 50");
+}
+
+if (6 != 3) {
+
+    console.log("executed");
+}
+
+// <, > , <=, >=, ==, !=, ===,!==
