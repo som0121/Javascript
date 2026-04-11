ce347b0e79f2ceb45f7f03bbf4efa995394742e7
som <somparashar007@gmail.com>
2026-04-11 22:12:38 +0530

    dowhile

---
+++ 05_iterations/dowhile.js
@@ -0,0 +1,29 @@
+
+let index = 0
+
+while (index <= 15) {
+
+    console.log(`value of index is ${index}`);
+    
+    index = index + 3
+}
+
+let myArray = ["sunday", "friday", "saturday"]
+
+let arr = 0
+
+while (arr < myArray.length){
+
+    console.log(`the day is ${myArray[arr]}`);
+    
+    arr = arr+1
+}
+
+let score = 1 
+
+do {
+
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score<=5);
