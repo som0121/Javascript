e3afeff78177b0fde5f2641333d08038eec10512
som <somparashar007@gmail.com>
2026-05-01 23:37:51 +0530

    props of getter setter

---
+++ 10_classes_and_oop/properties_getter_setter.js
@@ -0,0 +1,33 @@
+function User(email, password){
+
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email',{
+
+        get: function(){
+
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+
+            this._email = value
+        }
+    })
+
+     Object.defineProperty(this, 'password',{
+
+        get: function(){
+
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+
+            this._password = value
+        }
+    })
+}
+
+const som = new User("som@google.com", "abc")
+console.log(som.email);
+console.log(som.password);
