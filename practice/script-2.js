f04cefc203b5faaab88f48c4d908e3c322c79ea0
som <somparashar007@gmail.com>
2026-07-22 14:05:01 +0530

    timeout

--- practice/script-2.js
+++ practice/script-2.js
@@ -1,7 +1,17 @@
 console.log("start of script 2");
 
 setTimeout(()=>{
-    console.log("This is from the task queue(setTimeout)");
-},5000);
+    console.log("x");
+},0);
+
+setTimeout(()=>{
+    console.log("Y");
+},0);
+
+
+setTimeout(()=>{
+    console.log("z");
+},2*50);
 
 console.log("End of script");
+console.log("abc");
