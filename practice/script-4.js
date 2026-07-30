50db5323961f3f4a865cd71b42c33654462b3776
som <somparashar007@gmail.com>
2026-07-30 13:40:03 +0530

    object literals

---
+++ practice/script-4.js
@@ -0,0 +1,33 @@
+// objects
+
+const person = {
+
+    fname: "Som",
+    lname:"Parashar",
+    contact: "9898988989",
+
+    getname: function(){
+        console.log("Som");
+
+    },
+    getcontact(){},
+};
+
+const p2 = {
+     fname: "Rahul",
+    lname:"Sharma",
+    contact: "962889889",
+    getnanme: function(){
+        console.log("Rahul");
+    },
+};
+
+const p3 = {
+    fname: "abhay",
+    lname:"singh",
+    contact: "962889889",
+    getnanme: function(){
+        console.log("abhay");
+    },
+};
+console.log(person.getname())
