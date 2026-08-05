6161693a6cd2392634c50880753e67216c14f678
som <somparashar007@gmail.com>
2026-08-05 19:57:21 +0530

    proxy validation

--- practice/proxy.js
+++ practice/proxy.js
@@ -8,12 +8,28 @@ const p1Proxy = new Proxy(p1,{
     get(target,prop){
         if (prop in target) return target[prop];
         return false;
+
         
-        console.log({target,prop})
-        return 'testing';
+    },
+    set(target,prop,value){
+        if(!(prop in target)) throw new Error (`${prop} does not exists`);
+
+        switch(prop){
+            case 'fname':
+            case 'lname':
+                if(typeof value !== 'string') throw new Error(`${prop} must be a string`)
+            break
+            case 'age':
+                if(typeof value !== 'number') throw new Error(`${prop} must be a number`)
+                
+                if(value<= 0) throw new Error (`${prop} must be > zero`)
+                
+        }
+        target[prop] = value;
     },
 
 });
-p1Proxy.age = -22;
+p1Proxy.age = 22;
+p1Proxy.fname = 'som';
 
-console.log(p1Proxy.lname)
+console.log(p1Proxy.age); 
